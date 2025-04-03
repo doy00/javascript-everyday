9205668d3f1df8de0f35ef8692bd77345f38b27f
doy00 <dhkm201@gmail.com>
2025-04-03 23:56:03 +0900

    문제05 풀이

--- 2025/01/05-1.array.js
+++ 2025/01/05-1.array.js
@@ -196,5 +196,22 @@ function 수포자(answers) {
 function 행렬의곱셈(arr1, arr2) {
   // 행렬 arr1과 arr2의 행과 열의 수 
   const r1 = arr1.length;
-  const c1 = arr1[0].length;행려
+  const c1 = arr1[0].length;
+
+  // 결과를 저장할 2차원 배열 초기화
+  const ret = [];
+  for (let i = 0; i < r1; i++) {
+    ret.push(new Array(c2).fill(0));
+  }
+
+  // 첫번째 행렬 arr1의 각 행과 두번째 행렬 arr2의 각 열에 대해
+  for (let i = 0; i < r1; i++) {
+    for (let j = 0; j < c2; j++) {
+      // 두 행렬의 데이터를 곱해 결과 배열에 더해줌
+      for (let k = 0; k < c1; k++) {
+        ret[i][j] += arr1[i][k] * arr2[k][j];
+      }
+    }
+  }
+  return ret;
 }
